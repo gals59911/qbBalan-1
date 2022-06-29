aa2edc32953d32c6c32aa8993afb9ee48550999c
Галавтдинов Станислав Сергеевич ИВТ-21-1Б <91310045+gals59911@users.noreply.github.com>
2022-06-29 12:44:11 +0500

    Add files via upload

--- Код/Practic.ino
+++ Код/Practic.ino
@@ -1,16 +1,16 @@
 char income_symbol;
-void front()
+int value=0;
+void front(int value)
 {
  pinMode(3,INPUT);
  pinMode(6,INPUT);
  pinMode(5,OUTPUT);
  pinMode(11,OUTPUT);
  Serial.print("F");
- int value = 0;
- while(value!=255){
-  analogWrite(5,value);
-  analogWrite(11,value);
-  value += 10;
+ while(value!=30){
+  analogWrite(3,value);
+  analogWrite(6,value);
+  value+=25;
  }
 }
 void back(){
@@ -18,21 +18,21 @@ void back(){
  pinMode(11,INPUT);
  pinMode(6,OUTPUT);
  pinMode(3,OUTPUT);
- Serial.print("F");
+ Serial.print("B");
  int value = 0;
  while(value!=-255){
-  analogWrite(5,value);
-  analogWrite(11,value);
-  value -= 10;
+  analogWrite(3,value);
+  analogWrite(6,value);
+  value -= 15;
   }
 }
   void stop(){
     Serial.print("S");
       Serial.println(income_symbol, 1); // печатает в консоль символ
-      pinMode(3,INPUT);
-      pinMode(6,INPUT);
-      pinMode(5,INPUT);
-      pinMode(11,INPUT);
+      analogWrite(3,0);
+      analogWrite(6,0);
+      analogWrite(5,0);
+      analogWrite(11,0);
   }
   void leftfront(){
     pinMode(6,INPUT);
@@ -88,12 +88,15 @@ void loop(){
 while (Serial.available()>0)
  {
   income_symbol = Serial.read();
+  Serial.println(income_symbol);
   switch (income_symbol)
   {
     case 'F': // Движение вперед
-    front();
+    stop();
+    front(value);
     break;
     case 'B': // Движение назад
+    stop();
     back();
     break;
     case 'G': // Движение налево вперед
@@ -103,20 +106,30 @@ while (Serial.available()>0)
       stop();
       break;
     case 'I': // Движение вперед направо
+    stop();
       rightfront();
       break;
     case 'H': // Движение назад налево
+    stop();
       leftback();
       break;
     case 'J': // Движение назад направо
+    stop();
       rightback();
       break;
     case 'R': // Разворот направо 
+    stop();
       razvorotR();
       break;
     case 'L': // Разворот налево
+    stop();
       razvorotL();
       break;
+    case '1':
+    stop();
+    int value=5;
+    Serial.println(value);
+    break;
    }
   }
 }
